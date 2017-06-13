ae5a617be5230f61527acdc585b4d87af6cc6bd7
momo1002 <momoko.nakada1002@gmail.com>
2017-06-13 16:21:56 -0700

    fix compass, show distance

--- app/src/main/java/com/test/whereeeee/UserDetails.java
+++ app/src/main/java/com/test/whereeeee/UserDetails.java
@@ -8,16 +8,16 @@ public class UserDetails {
 //    static String latitude = "";
 //    static String longitude = "";
 
-//    static float latitude1 = 49.2860f; // water front station
-//    static float longitude1 = -123.1117f; // water front station
+    static float latitude1 = 49.2860f; // water front station
+    static float longitude1 = -123.1117f; // water front station
+
+    static float latitude2 = 49.2798f; // vancouver library
+    static float longitude2 = -123.1157f; // vancouver library
+
+//    static float latitude2 = 49.2860f; // water front station
+//    static float longitude2 = -123.1117f; // water front station
 //
-//    static float latitude2 = 49.2798f; // vancouver library
-//    static float longitude2 = -123.16f; // vancouver library
-
-    static float latitude2 = 49.2860f; // water front station
-    static float longitude2 = -123.1117f; // water front station
-
-    static float latitude1 = 49.2798f; // vancouver library
-    static float longitude1 = -123.16f; // vancouver library
+//    static float latitude1 = 49.2798f; // vancouver library
+//    static float longitude1 = -123.1157f; // vancouver library
 
 }
